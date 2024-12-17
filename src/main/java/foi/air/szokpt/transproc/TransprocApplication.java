0484cf51095cecb1619af14d5c238218f0d32a7a
NinoLoncar <nino.loncar16@gmail.com>
2024-12-17 19:22:54 +0100

    Inicijalni projekt

---
+++ src/main/java/foi/air/szokpt/transproc/TransprocApplication.java
@@ -0,0 +1,13 @@
+package foi.air.szokpt.transproc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransprocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransprocApplication.class, args);
+	}
+
+}
