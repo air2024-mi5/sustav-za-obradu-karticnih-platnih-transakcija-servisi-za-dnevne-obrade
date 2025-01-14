15451a8cf3fec95f7ba9bde8fd646e935ed256a9
NinoLoncar <nino.loncar16@gmail.com>
2025-01-14 11:18:31 +0100

    MI5-74 Dodano dohvaćanje transakcija za obradu

--- src/main/java/foi/air/szokpt/transproc/services/ProcessingService.java
+++ src/main/java/foi/air/szokpt/transproc/services/ProcessingService.java
@@ -2,9 +2,13 @@ package foi.air.szokpt.transproc.services;
 
 import foi.air.szokpt.transproc.clients.TransactionClient;
 import foi.air.szokpt.transproc.dtos.Transaction;
+import foi.air.szokpt.transproc.exceptions.TransactionProcessingException;
+import foi.air.szokpt.transproc.models.SelectedTransaction;
+import foi.air.szokpt.transproc.models.TransactionProcessing;
+import foi.air.szokpt.transproc.repositories.SelectedTransactionRepository;
+import foi.air.szokpt.transproc.repositories.TransactionProcessingRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,13 +16,43 @@ import java.util.UUID;
 public class ProcessingService {
 
     private TransactionClient transactionClient;
+    private TransactionProcessingRepository transactionProcessingRepository;
+    private SelectedTransactionRepository selectedTransactionRepository;
 
-    public ProcessingService(TransactionClient transactionClient) {
+    public ProcessingService(TransactionClient transactionClient,
+                             TransactionProcessingRepository transactionProcessingRepository,
+                             SelectedTransactionRepository selectedTransactionRepository) {
         this.transactionClient = transactionClient;
+        this.transactionProcessingRepository = transactionProcessingRepository;
+        this.selectedTransactionRepository = selectedTransactionRepository;
     }
 
 
     public void startDailyProcessing() {
+        TransactionProcessing scheduledProcessing;
+        try {
+            scheduledProcessing = getScheduledProcessing();
+        } catch (Exception e) {
+            return;
+        }
+        List<SelectedTransaction> selectedTransactions = selectedTransactionRepository
+                .findByTransactionProcessingId(scheduledProcessing.getId());
+        List<UUID> guids = selectedTransactions.stream()
+                .map(SelectedTransaction::getGuid)
+                .toList();
+        
+        List<Transaction> transactions = transactionClient.fetchTransactions(guids);
 
     }
+
+    private TransactionProcessing getScheduledProcessing() {
+        TransactionProcessing scheduledProcessing =
+                transactionProcessingRepository.findScheduledTransactionProcessing()
+                        .stream().
+                        findFirst().
+                        orElseThrow(() -> new TransactionProcessingException(
+                                "No scheduled processing found"));
+        return scheduledProcessing;
+    }
+
 }
