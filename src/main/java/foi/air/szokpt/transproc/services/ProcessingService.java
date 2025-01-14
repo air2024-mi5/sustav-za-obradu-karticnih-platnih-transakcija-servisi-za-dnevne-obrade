6ec4ef50f3f9218d387eab75ce6e1ce47058050f
NinoLoncar <nino.loncar16@gmail.com>
2025-01-14 22:54:31 +0100

    MI5-74 Pokretanje obrade dodanou ProcessingService

--- src/main/java/foi/air/szokpt/transproc/services/ProcessingService.java
+++ src/main/java/foi/air/szokpt/transproc/services/ProcessingService.java
@@ -3,8 +3,10 @@ package foi.air.szokpt.transproc.services;
 import foi.air.szokpt.transproc.clients.TransactionClient;
 import foi.air.szokpt.transproc.dtos.Transaction;
 import foi.air.szokpt.transproc.exceptions.TransactionProcessingException;
+import foi.air.szokpt.transproc.models.BatchRecord;
 import foi.air.szokpt.transproc.models.SelectedTransaction;
 import foi.air.szokpt.transproc.models.TransactionProcessing;
+import foi.air.szokpt.transproc.repositories.BatchRecordRepository;
 import foi.air.szokpt.transproc.repositories.SelectedTransactionRepository;
 import foi.air.szokpt.transproc.repositories.TransactionProcessingRepository;
 import org.springframework.stereotype.Service;
@@ -15,16 +17,22 @@ import java.util.UUID;
 @Service
 public class ProcessingService {
 
-    private TransactionClient transactionClient;
-    private TransactionProcessingRepository transactionProcessingRepository;
-    private SelectedTransactionRepository selectedTransactionRepository;
+    private final TransactionClient transactionClient;
+    private final TransactionProcessingRepository transactionProcessingRepository;
+    private final SelectedTransactionRepository selectedTransactionRepository;
+    private final TransactionProcessingEngine transactionProcessingEngine;
+    private final BatchRecordRepository batchRecordRepository;
 
     public ProcessingService(TransactionClient transactionClient,
                              TransactionProcessingRepository transactionProcessingRepository,
-                             SelectedTransactionRepository selectedTransactionRepository) {
+                             SelectedTransactionRepository selectedTransactionRepository,
+                             TransactionProcessingEngine transactionProcessingEngine,
+                             BatchRecordRepository batchRecordRepository) {
         this.transactionClient = transactionClient;
         this.transactionProcessingRepository = transactionProcessingRepository;
         this.selectedTransactionRepository = selectedTransactionRepository;
+        this.transactionProcessingEngine = transactionProcessingEngine;
+        this.batchRecordRepository = batchRecordRepository;
     }
 
 
@@ -40,9 +48,14 @@ public class ProcessingService {
         List<UUID> guids = selectedTransactions.stream()
                 .map(SelectedTransaction::getGuid)
                 .toList();
-        
-        List<Transaction> transactions = transactionClient.fetchTransactions(guids);
 
+        List<Transaction> transactions = transactionClient.fetchTransactions(guids);
+        List<BatchRecord> processedBatches = transactionProcessingEngine.processTransactions(transactions);
+
+        for (BatchRecord record : processedBatches) {
+            record.setTransactionProcessing(scheduledProcessing);
+            batchRecordRepository.save(record);
+        }
     }
 
     private TransactionProcessing getScheduledProcessing() {
@@ -54,5 +67,4 @@ public class ProcessingService {
                                 "No scheduled processing found"));
         return scheduledProcessing;
     }
-
 }
