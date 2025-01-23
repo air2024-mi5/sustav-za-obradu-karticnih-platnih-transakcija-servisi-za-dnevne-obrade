94ee0f9ee44a8bf35c58a03315914b2ef4c0c0a0
NinoLoncar <nino.loncar16@gmail.com>
2025-01-23 22:29:00 +0100

    MI5-112 Proširent SelectedTransacitonRepository
    Dodana metoda za brisanje svih odabranih transakcija za neku obradu

--- src/main/java/foi/air/szokpt/transproc/repositories/SelectedTransactionRepository.java
+++ src/main/java/foi/air/szokpt/transproc/repositories/SelectedTransactionRepository.java
@@ -1,6 +1,7 @@
 package foi.air.szokpt.transproc.repositories;
 
 import foi.air.szokpt.transproc.models.SelectedTransaction;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,7 @@ import java.util.UUID;
 
 public interface SelectedTransactionRepository extends JpaRepository<SelectedTransaction, UUID> {
     List<SelectedTransaction> findByTransactionProcessingId(Integer transactionProcessingId);
+
+    @Transactional
+    void deleteByTransactionProcessingId(Integer transactionProcessingId);
 }
